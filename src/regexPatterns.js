18afc78b3fa64db7f5da2c78c33c039bc4dc321d
ishejaladouce <l.isheja@alustudent.com>
2025-09-22 14:22:46 +0200

    add time regex + tests for 12-hours and 24-hour formats

--- src/regexPatterns.js
+++ src/regexPatterns.js
@@ -4,6 +4,7 @@ const email = /\b[A-Za-z0-9._%=-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
 const url = /https?:\/\/[^\s/$.?#].[^\s]*/gi;
 const phone = /(\(\d{3}\)\s?|\d{3}[-.])\d{3}[-.]\d{4}/g;
 const creditCard = /\b\d{4}[- ]\d{4}[- ]\d{4}[- ]\d{4}\b/g;
+const time = /\b((0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]|(0?[1-9]|1[0-2]):[0-5][0-9]\s?(AM|PM|am|pm))\b/g;
 
 // #Export patterns so that the test files can use them
-module.exports = { email, url, phone, creditCard };
+module.exports = { email, url, phone, creditCard, time };
---
+++ tests/testTimes.js
@@ -0,0 +1,17 @@
+// tests/testTimes.js
+const { time } = require('../src/regexPatterns');
+
+// Sample inputs
+const samples = [
+  'Meeting at 14:30',
+  'Wake up at 2:30 PM',
+  'Alarm: 12:00 am',
+  'Invalid: 25:00 should not match'
+];
+
+// Run tests
+samples.forEach(str => {
+  console.log('Input:', str);
+  console.log('Matches:', str.match(time) || []);
+  console.log('---');
+});
